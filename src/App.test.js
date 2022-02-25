a133d2d9c6d995fcedac9523abde14e2cc7002d6
CarolynnFleming <cfcatpink@gmail.com>
2022-02-24 17:23:27 -0800

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { makeColumns, makeCountByAge } from './data-utils';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/johnny/i);
-  expect(linkElement).toBeInTheDocument();
+  const expected = [
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'city', headerName: 'City', width: 150 },
+    { field: 'favorite_food', headerName: 'Favorite Food', width: 150 },
+  ];
+
+  const actual = makeColumns([
+    { name: 'jen', city: 'wenquan', favorite_food: 'pizza', },
+    { name: 'mark', city: 'lupon', favorite_food: 'burger', },
+    { name: 'travis', city: 'youghal', favorite_food: 'tacos', },
+  ]);
+
+  expect(actual).toEqual(expected);
+});
+
+test('renders learn react link', () => {
+  const expected = [
+    { food: 'pizza', AverageAAge: 20 }
+  ];
+
+  const actual = makeCountByAge([
+    { favorite_food: 'pizza', animal_age: 12, },
+    { favorite_food: 'pizza', animal_age: 20, },
+    
+    
+  ]);
+
+  expect(actual).toEqual(expected);
 });
